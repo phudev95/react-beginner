ab847580fc4440a9187253a40cba6451eb0e06a5
Phu Phan <phudev95@gmail.com>
2016-11-19 11:23:04 +0700

    1. Persisting Order State with localstorage

--- src/components/App.js
+++ src/components/App.js
@@ -24,14 +24,27 @@ class App extends React.Component {
     }
 
     componentWillMount () {
+        // This runs right before the <App/> is rendered
         this.ref = base.syncState(`${this.props.params.storeId}/fishes`,{
             context: this,
             state: 'fishes'
         });
+
+        // Check if there is any order in localStorage
+        const localStorageRef = localStorage.getItem(`order-${this.props.params.storeId}`);
+        if (localStorageRef) {
+            this.setState({
+                order: JSON.parse(localStorageRef)
+            });
+        }
     }
 
     componentWillUnmount () {
         base.removeBinding(this.ref);
+    }
+
+    componentWillUpdate (nextProps, nextState) {
+        localStorage.setItem(`order-${this.props.params.storeId}`, JSON.stringify(nextState.order));
     }
 
     addFish (fish) {
